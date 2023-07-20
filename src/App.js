1f50fe92f6aa4ad51b1714eefb0fc0990754bdce
Ahmed <himozhima@gmail.com>
2023-07-20 18:43:16 +0300

    getting question from json server ()

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React from "react";
-import DateCounter from './DateCounter';
+import React, { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 export default function App() {
-  return(
-    <DateCounter />
-  )
+  const initialState = {
+    questions: [],
+
+    // error ,ready ,active ,finished
+    status: "loading",
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err)=> dispatch({type:"dataFaild "}))
+  }, []);
+  
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return { ...state, questions: action.payload, status: "ready" };
+      case "dataFaild":
+        return {...state,status:"Error"}  
+      default:
+        throw new Error("Action unknown");
+    }
+  }
+  
+  return (
+    <div className="App">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
